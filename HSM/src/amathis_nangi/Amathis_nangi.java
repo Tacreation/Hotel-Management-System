e890f732611f6aece6c902fa4f2bdb20636703e4
Tacreation <31962625+Tacreation@users.noreply.github.com>
2020-12-12 20:55:03 +0530

    Add files via upload

---
+++ HSM/src/amathis_nangi/Amathis_nangi.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package amathis_nangi;
+
+/**
+ *
+ * @author Owner
+ */
+public class Amathis_nangi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
